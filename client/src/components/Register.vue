272c5f3f4b4ce869aa1448a4ad9f70f49ff7ac14
Aaron Cuddeback <aaron.cuddeback@gymratpacks.com>
2018-05-13 22:29:01 -0700

    added vuetify and backend authtication error messages

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,25 @@
 
 <template>
-<div>
-<h1>Register</h1>
+  <v-layout row wrap>
+    <v-flex xs8 offset-xs2>
+      <div class="white elevation-2"> 
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
 
-<input type ="email" name="email" v-model="email" placeholder="email" />
-<br>
-<input type ="password" name="password" v-model="password" placeholder="password" />
-<br>
-<button @click="register">Register</button>
-</div>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type ="email" name="email" v-model="email" placeholder="email" />
+        <br>
+        <input type ="password" name="password" v-model="password" placeholder="password" />
+        <br>
+        <div class="error" v-html="error" />
+        <br>
+        <v-btn @click="register">Register</v-btn>
+        </div>
+      </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -23,11 +34,14 @@ export default {
   },
   methods: {
     async register () {
+      try {
         const response = await AuthenticationService.register({
           email: this.email,
           password: this.password
         })
-      console.log(response.data)
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 };
@@ -35,5 +49,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .error {
+    color: red;
+  }
 </style>
